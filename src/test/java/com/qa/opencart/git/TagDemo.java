c5319486e0881c3404a6adaa2eb1ed698978c3ef
avnisshsingh <avnisshsingh45@gmail.com>
2026-02-04 18:07:17 +0530

    version 2

--- src/test/java/com/qa/opencart/git/TagDemo.java
+++ src/test/java/com/qa/opencart/git/TagDemo.java
@@ -1,7 +1,12 @@
 package com.qa.opencart.git;
 
 public class TagDemo {
+	
 	public void version1() {
 		System.out.println("Version 1");
+	}
+	
+	public void version2() {
+		System.out.println("Version 2");
 	}
 }
