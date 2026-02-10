ece748487420044cd17b35cef67027fbe02f0a50
avnisshsingh <avnisshsingh45@gmail.com>
2026-02-10 18:27:45 +0530

    removed junk

--- src/test/java/com/qa/opencart/git/TestPage.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.git;
-
-public class TestPage {
-
-	public void version1() {
-		System.out.println("Version 1 ===  From Remote");
-		System.out.println("Version 1 ===  From Local");
-	}
-}
